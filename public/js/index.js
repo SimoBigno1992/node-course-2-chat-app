f1551d4aa3305cc19310899b4514d0d95d1ad012
simone bignotti <s.bignotti@reply.it>
2019-01-21 10:43:24 +0100

    added callback to emitter

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,10 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
   console.log('newMessage', message);
 });
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'Hi!'
+}, (data) => {
+  console.log('Got it.', data);
+});
